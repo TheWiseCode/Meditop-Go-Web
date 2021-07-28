03f2f8736758a9b38210902328ccadbac1dcc352
Willy Vargas <willy4k@gmail.com>
2021-07-27 21:16:46 -0400

    Correo verf

--- routes/web.php
+++ routes/web.php
@@ -36,17 +36,17 @@ Auth::routes();
 //----------VERIFICACION DE CORREO
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+})->middleware('auth:sanctum')->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
     return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+})->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+})->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
 //-----------------------------
 
 
