a0fb34de87a81ad9fc2f1008990f834410ac56db
Willy Vargas <willy4k@gmail.com>
2021-08-03 16:50:50 -0400

    Deploy

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
             //dd('bien');
             return $this->sendLoginResponse($request);
         }
-        dd('mal');
+        //dd('mal');
 
         $this->incrementLoginAttempts($request);
 
