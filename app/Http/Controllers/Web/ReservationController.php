4995c5096769becac01413f0f07d22a29b6cefb0
Willy Vargas <willy4k@gmail.com>
2021-08-05 02:34:17 -0400

    Api verificar reserva

--- app/Http/Controllers/Web/ReservationController.php
+++ app/Http/Controllers/Web/ReservationController.php
@@ -4,81 +4,42 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Reservation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Reservation $reservation
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Reservation $reservation
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Reservation $reservation
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Reservation $reservation)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Reservation $reservation
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Reservation $reservation)
     {
         //
@@ -93,7 +54,7 @@ class ReservationController extends Controller
         ]);
         $datetime = $data['date'] . ' ' . $data['time'] . ':00';
         $reserve = Reservation::join('offer_specialties', 'offer_specialties.id', 'reservations.id_offer')
-            ->where('time_consult', $datetime)->get();
+            ->where('time_consult', $datetime)->first();
         if ($reserve) {
             return response(['message' => 'Horario no disponible'], 406);
         }
