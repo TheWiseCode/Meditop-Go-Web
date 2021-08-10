7a595cf4f5428229a8d6fc4d5c785e57f31c3ff9
Willy Vargas <willy4k@gmail.com>
2021-08-09 20:32:08 -0400

    Notification cancelar

--- resources/views/doctor/reservations/index.blade.php
+++ resources/views/doctor/reservations/index.blade.php
@@ -166,13 +166,13 @@
             $('#deniedIdRes').val(id);
         }
 
-        $('#exampleModal').on('show.bs.modal', function (event) {
+        /*$('#exampleModal').on('show.bs.modal', function (event) {
             var button = $(event.relatedTarget) // Button that triggered the modal
             var recipient = button.data('whatever') // Extract info from data-* attributes
             var modal = $(this)
-            modal.find('.modal-title').text('New message to ' + recipient)
-            modal.find('.modal-body input').val(recipient)
-        })
+            //modal.find('.modal-title').text('New message to ' + recipient)
+            //modal.find('.modal-body input').val(recipient)
+        })*/
     </script>
     <script>
         $(document).ready(function () {
